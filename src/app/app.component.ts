cb91c6d6729871557bda743abc8bb9ab908c47ca
daliyajohn <daliyajohn75@gmail.com>
2018-02-11 20:52:59 +0530

    Type declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public name; // type must be any
+  public name1: number; // type must be number
+  public name3: any = [1, 2, 3]; // must be array
+  public nameList: Array<string> = []; // array contain string value
+  public nameList2: Array<number> = []; // array contain number value
+
   title = 'app';
   constructor() {
+    enum color {daliya, seeniya, viji} //  constant value , default value should be 0, 1 , 2 etc
+    console.log(color.daliya);
   }
 
   myFunction() {
+    const age = 6;
     for (let i = 0; i < 4; i++) {
       let daliya = 1;
       console.log(i); // actual result
