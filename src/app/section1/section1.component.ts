8272be0b97105a65160ff642dffbcf2ade112e00
daliyajohn <daliyajohn75@gmail.com>
2018-02-14 23:54:48 +0530

    Like and dislike

--- src/app/section1/section1.component.ts
+++ src/app/section1/section1.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section1.component.css']
 })
 export class Section1Component implements OnInit {
+  public isSelected: Boolean = false;
+  public like: any = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  thumpChange() {
+    this.isSelected = !this.isSelected;
+    if (this.isSelected) {
+      this.like++;
+    } else {
+      this.like--;
+    }
+  }
 }
